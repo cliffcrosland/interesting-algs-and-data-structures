78591d41d91126921e39e1356cca22a183519b0a
Cliff Crosland <cliffcrosland@gmail.com>
2015-05-13 09:47:37 -0700

    Implement quickselect.

--- src/com/cliffcrosland/Main.java
+++ src/com/cliffcrosland/Main.java
@@ -1,22 +1,13 @@
 package com.cliffcrosland;
 
-
-import com.cliffcrosland.heap.MinHeap;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import com.cliffcrosland.quickselect.Quickselect;
 
 public class Main {
 
     public static void main(String[] args) {
-        List<Integer> values = Arrays.asList(4, 1, 45, 12, 3, 6, 23, 5, 6);
-        MinHeap<Integer> heap = new MinHeap<Integer>(values);
-        while (heap.size() > 0) {
-            System.out.println(heap.popMin());
+        Integer[] values = new Integer[] {4, 1, 45, 12, 3, 6, 23, 5, 6};
+        for (int n = values.length; n >= 1; n--) {
+            System.out.println(Quickselect.select(values, n));
         }
-
-
-
     }
 }
