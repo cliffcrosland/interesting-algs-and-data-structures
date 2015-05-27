03eb706d7fa4408f50f1a8b293b90877cd1d84bc
Cliff Crosland <cliffcrosland@gmail.com>
2015-05-27 10:08:23 -0700

    Add new Graph constructor, can construct from nodes alone

--- src/com/cliffcrosland/graph/Graph.java
+++ src/com/cliffcrosland/graph/Graph.java
@@ -1,5 +1,6 @@
 package com.cliffcrosland.graph;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Graph {
@@ -7,8 +8,20 @@ public class Graph {
     public Set<GraphNode> nodes;
     public Set<GraphEdge> edges;
 
+    public Graph(Set<GraphNode> nodes) {
+        Set<GraphEdge> edges = new HashSet<GraphEdge>();
+        for (GraphNode node : nodes) {
+            for (GraphEdge edge : node.edges) {
+                edges.add(edge);
+            }
+        }
+        this.nodes = nodes;
+        this.edges = edges;
+    }
+
     public Graph(Set<GraphNode> nodes, Set<GraphEdge> edges) {
         this.nodes = nodes;
         this.edges = edges;
     }
+
 }
